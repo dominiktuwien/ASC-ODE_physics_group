633339fd94696454fde427457682a07b73cd9f7b
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2023-12-05 09:46:42 +0100

    Runge-Kutta time-stepping

--- demos/test_ode.cc
+++ demos/test_ode.cc
@@ -24,13 +24,66 @@ class MassSpring : public NonlinearFunction
 };
 
 
+Matrix<> Gauss2a { { 0.25, 0.25 - sqrt(3)/6 }, { 0.25 + sqrt(3)/6, 0.25 } };
+Vector<> Gauss2b { 0.5, 0.5 };
+Vector<> Gauss2c { 0.5 - sqrt(3)/6, 0.5 + sqrt(3)/6 };
+
+Vector<> Gauss3c { 0.5 - sqrt(15)/10, 0.5, 0.5+sqrt(15)/10 };
+
+
+auto ComputeABfromC (const Vector<> & c)
+{
+  int s = c.Size();
+  Matrix M(s, s);
+  Vector tmp(s);
+  
+  for (int i = 0; i < s; i++)
+    for (int j = 0; j < s; j++)
+      M(i,j) = std::pow(c(j), i);
+  CalcInverse(M);
+  
+  for (int i = 0; i < s; i++)
+    tmp(i) = 1.0 / (i+1);
+
+  Vector b = M * tmp;
+  cout << "b = " << b << endl;
+
+  Matrix a(s,s);
+
+  for (int j = 0; j < s; j++)
+    {
+      for (int i = 0; i < s; i++)
+        tmp(i) = std::pow(c(j),i+1) / (i+1);
+      a.Row(j) = M * tmp;
+    }
+  /*
+  cout << "b = " << b << endl;
+  cout << "a = " << a << endl;
+  */
+  return tuple { a, b };
+}
+  
+
+
 int main()
 {
   double tend = 4*M_PI;
-  int steps = 100;
+  int steps = 200;
   Vector<> y { 1, 0 };
   auto rhs = make_shared<MassSpring>();
-  
+
+  /*
   SolveODE_IE(tend, steps, y, rhs,
               [](double t, VectorView<double> y) { cout << t << "  " << y(0) << " " << y(1) << endl; });
+  */
+
+  auto [a,b] = ComputeABfromC (Gauss3c);
+  SolveODE_RK(tend, steps, a, b, Gauss3c, y, rhs, 
+              [](double t, VectorView<double> y) { cout << t << "  " << y(0) << " " << y(1) << endl; });
+
+  
+  /*
+  SolveODE_RK(tend, steps, Gauss2a, Gauss2b, Gauss2c, y, rhs, 
+              [](double t, VectorView<double> y) { cout << t << "  " << y(0) << " " << y(1) << endl; });
+  */
 }
